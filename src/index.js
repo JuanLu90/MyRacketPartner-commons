07c3ff711b92723ebe97b297871c6e42bb0c6c3f
Juan Luis <juanlu.calderon@gmail.com>
2024-08-09 16:58:56 +0200

    apiutils added

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as matchesResolvers } from "./resolvers/matches.resolvers";
 export { default as colors } from "./utils/stylesUtil";
+export { default as apiUtils } from "./utils/apiUtils";
