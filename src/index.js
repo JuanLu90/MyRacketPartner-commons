31a66b78e22f3b84e54d2464a99215b5cb93b4f1
Juan Luis <juanlu.calderon@gmail.com>
2024-08-09 17:23:03 +0200

    apiutils deleted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 export { default as matchesResolvers } from "./resolvers/matches.resolvers";
 export { default as colors } from "./utils/stylesUtil";
-export { default as apiUtils } from "./utils/apiUtils";
